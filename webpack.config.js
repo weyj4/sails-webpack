e422022bd288a5f89fd688e1fc4b6e8e1aa7d42b
Joey Di Nardo <yejodido@gmail.com>
2015-10-18 11:50:25 -0400

    script change and removed sails io js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoErrorsPlugin()
   ],
   module: {
     loaders: [
